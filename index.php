51cc7d69a1f07df1857e333d630ef39bd5bc7e7c
talisepatu <12ibnu.mdz@gmail.com>
2023-02-09 19:18:38 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Coba Git Remote</title>
+</head>
+<body>
+	<h1>Hellow World!</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam odio ad obcaecati, omnis quod quisquam rerum asperiores laudantium molestiae repellat ea animi, debitis accusamus dicta harum veniam? Pariatur, quidem illo.</p>
+</body>
+</html>

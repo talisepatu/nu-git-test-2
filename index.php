c34f245bfdc1bd6d532ed7c56bd596e5341fd4d9
talisepatu <12ibnu.mdz@gmail.com>
2023-02-09 19:18:59 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 </head>
 <body>
 	<h1>Hellow World!</h1>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam odio ad obcaecati, omnis quod quisquam rerum asperiores laudantium molestiae repellat ea animi, debitis accusamus dicta harum veniam? Pariatur, quidem illo.</p>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam odio ad obcaecati, omnis quod quisquam rerum asperiores laudantium molestiae repellat ea animi, debitis accusamus dicta harum veniam? Pariatur, quidem illos.</p>
 </body>
 </html>
